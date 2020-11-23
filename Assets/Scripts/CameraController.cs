539adf6f16409db0a341366c9ff6018963cc5262
Chika0IQ <mohdkhairin62@gmail.com>
2020-11-23 11:52:06 +0800

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    Vector3 offset = new Vector3(0.05f, 2.19f,-2.61f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, 0.1f);
+    }
+
+    
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float moveSpeed = 5.0f;
+
+
+    public Animator playerAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+    void PlayerMovement()
+    {
+        /*if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerAnim.SetBool("isRun", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            playerAnim.SetBool("isRun", false);
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAnim.SetTrigger("triggJump");
+        }
+
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            
+            StartRun();
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            
+            StartRun();
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            playerAnim.SetBool("isRun", false);
+        }
+        
+
+    }
+    void StartRun()
+    {
+        playerAnim.SetBool("isRun", true);
+        playerAnim.SetFloat("startRun", 0.26f);
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("TagCone"))
+        {
+            Debug.Log("Adafaf");
+        }
+    }
+}
